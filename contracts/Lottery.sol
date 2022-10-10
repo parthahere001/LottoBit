7502ad70b41f3f650b20678fb6d94fae25989e30
parthahere001 <parthahere001.com>
2022-10-10 18:03:32 +0530

    Set up the Price Feed using Chainlink smartcontractkit Github Repo

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,12 +1,14 @@
-pragma solidity ^0.6.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
 
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 contract Lottery{
     address payable[] public players;
     uint256 public usdEntryFee;
     AggregatorV3Interface internal ethUsdPriceFeed;
 
 
-    constructor(address _priceFeeAddress) public {
+    constructor (address _priceFeeAddress) public {
         usdEntryFee = 50 * (10**18);
         ethUsdPriceFeed = AggregatorV3Interface(_priceFeeAddress);
     }
@@ -16,7 +18,7 @@ contract Lottery{
     function enter() public payable {
         // 50 Dollars minimum
         //require();
-        players.push(msg.sender);
+        players.push(payable(msg.sender));
     }
 
     function getEnteranceFee() public view returns (uint256){}
